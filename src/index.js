21061e2cccebc1571f0f8bd9fc4f15bf958169e4
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-09-18 15:45:42 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector('form').addEventListener('submit',(e) => {
+  document.querySelector('#submit_button').addEventListener('click',(e) => {
     e.preventDefault();
-    buildToDo(document.querySelector('#new-task-description').value);
-    buildDueDate(document.querySelector('#add_due_date').value);
-  })
-});
+    buildToDo(document.querySelector('#new-task-description').value = 'No chores',document.querySelector('#add_due_date').value = 'No due date');
+    })
+  });
 
-function buildToDo(todo) {
+function buildToDo(todo,date) {
   let p = document.createElement('p');
   let btn = document.createElement('button');
   btn.addEventListener('click',handleDelete);
-  p.textContent = `${todo} `;
-  btn.textContent = ' x';
+  p.textContent = `${todo} --- ${date}  `;
+  btn.textContent = 'X';
   document.querySelector('#tasks').appendChild(p);
   p.appendChild(btn);
 }
 
-function buildDueDate(dueDate) {
-  let p = document.createElement('p');
-  p.textContent = dueDate;
-  document.querySelector('#tasks').appendChild(p);
-}
-
 function handleDelete(e) {
   e.target.parentNode.remove()
-}
+};
