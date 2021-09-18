fff7dbea171f4bfafcb0c34d6502c2cc53e46685
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-09-18 12:22:33 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit',(e) => {
+    e.preventDefault();
+    buildToDo(document.querySelector('#new-task-description').value);
+  })
 });
+
+function buildToDo(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click',handleDelete);
+  p.textContent = `${todo} `;
+  btn.textContent = ' x';
+  document.querySelector('#tasks').appendChild(p);
+  p.appendChild(btn);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
